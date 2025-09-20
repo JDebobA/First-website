4d43ebe676b57ea0b4578ced6c0c769aef83d2fe
SH_JDebobA <deniszabskij430@gmail.com>
2025-09-20 17:02:39 +0700

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,4 +1,6 @@
 function checkEmail(){
-    let email = document.querySelector('#emailField');
-    alert(email.value);
+    let email = document.querySelector('#emailField').value;
+    if(!email.includes('@')) alert('Нет символа @');
+    else if(!email.includes('.')) alert('Нет символа .');
+    else alert ("Всё норм");
 }
